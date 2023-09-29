53f16f9a652df41f2bd3bd0a1c65a33cb86d663e
l4c13w <l4c13w@gmail.com>
2023-09-29 01:59:04 -0300

    correção de uns problemas que o git deu

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -21,20 +21,19 @@ public class Aplicacao {
     public void exibirPainel(){
         System.out.println("\n");
         System.out.println("+---------------------------------------------------------------------------------------+");
-        System.out.println("|                                   PAINEL DE DADOS                                     |");
+        System.out.println("|                                        PAINEL DE DADOS                                |");
         System.out.println("+---------------------------------------------------------------------------------------+");
-        System.out.println("| FUNCIONARIO ATUALMENTE LOGADO: " + usr.getNome() + "                                 |\n" +
-                           "| VALOR TOTAL DOS CUSTOS DO MÊS ATUAL: "+ "                                           |\n" +
-                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE T.I.: "+"                     |\n" +
-                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE R.H.: "+"                     |\n" +
-                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. FINANCEIRO: "+"                  |\n" +
-                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. ADMINISTRATIVO: "+"              |\n" +
-                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE MARKETING: "+"                |\n" +
-                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE ENGENHARIA: "+"               |\n" +
-                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE VENDAS: "+"                   |\n" +
-                           "| OS 3 FUNCIONARIOS COM A MAIOR SOMA DE CUSTOS: "+"                                   |\n" +
-                           "| 99 - Sair                                                                             |");
-        System.out.println("+-----------------------------------------------------------------------------------+\n\n");
+        System.out.println("| FUNCIONARIO ATUALMENTE LOGADO:                                                        |\n" +
+                           "| VALOR TOTAL DOS CUSTOS DO MÊS ATUAL:                                                  |\n" +
+                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE T.I.:                           |\n" +
+                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE R.H.:                           |\n" +
+                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. FINANCEIRO:                        |\n" +
+                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. ADMINISTRATIVO:                    |\n" +
+                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE MARKETING:                      |\n" +
+                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE ENGENHARIA:                     |\n" +
+                           "| VALOR TOTAL DOS CUSTOS DOS ÚLTIMOS 3 MESES DO DEP. DE VENDAS:                         |\n" +
+                           "| OS 3 FUNCIONARIOS COM A MAIOR SOMA DE CUSTOS:                                         |");
+        System.out.println("+---------------------------------------------------------------------------------------+\n\n");
     }
 
     public void executar(){
@@ -43,14 +42,15 @@ public class Aplicacao {
         System.out.println("Digite seu nome de usuário para continuar: ");
         funcionarios.listarFuncionarios();
         usuario = funcionarios.login(in.nextLine());
-        System.out.println("Login efetuado com sucesso");
-        registraFuncionário();
-        System.out.println(funcionarios.listarFuncionarios());
-        System.out.println(departamentos.listarDepartamentos());
+        exibirPainel();
+        //System.out.println("Login efetuado com sucesso");           Ainda n funciona, o login deveria retornar um true quando funciona ou algo do tipo n sei
+        //registraFuncionario();
+        //System.out.println(funcionarios.listarFuncionarios());
+        //System.out.println(departamentos.listarDepartamentos());
 
     }
 
-    public boolean registraFuncionário(){
+    public boolean registraFuncionario(){
         System.out.println("Cadastro de Funcionário");
         System.out.println("Informe a Matrícula: ");
         String matricula = in.nextLine();
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
         System.out.println("Hello world!");
-=======
         Aplicacao app = new Aplicacao();
         app.executar();
->>>>>>> release/1.0
-=======
-        Aplicacao app = new Aplicacao();
-        app.executar();
->>>>>>> refs/remotes/origin/develop
     }
 }
